4a943a3782019c76521f7fdaafbe0772b9a50fe0
Joshua Rodriguez <joshrodriguez17@gmail.com>
2017-07-17 19:02:05 -0500

    Complete first draft of website, added SCSS and JS

---
+++ js/script.js
@@ -0,0 +1,93 @@
+$(function() {
+
+	// Get the form.
+	var form = $('#contactform');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#name').val('');
+			$('#email').val('');
+			$('#message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
+	});
+
+});
+
+$(window).on('load', function() {
+
+	$('#more').click(function() {
+		$('.nav-dropdown').slideToggle(400);
+		if ($(window).scrollTop() < 100) {
+			navChanges();
+		}
+
+	});
+
+	$(window).resize(function() {
+		if($(window).width() > 680) {
+			$('.nav-dropdown').hide();
+		}
+	});
+
+	$(window).scroll( function() {
+		if ($(window).scrollTop() >= 100) {
+			navChanges();
+		}
+		else {
+			navRevert();
+		}
+    	if ($('.nav-dropdown').is(":visible")) {
+    		$('.nav-dropdown').fadeOut(200);
+    	}
+    });
+});
+
+function navChanges() {
+		
+	$('.navbar').addClass('scrolled-nav')
+	$(".logo-link img").attr("src","images/montoyalogoblk.png");
+	$('#more img').attr("src","images/moreblk.svg");
+}
+
+function navRevert() {
+	$('.navbar').removeClass('scrolled-nav');
+	$(".logo-link img").attr("src","images/montoyalogowht.png");
+	$('#more img').attr("src","images/morewht.svg");
+}
